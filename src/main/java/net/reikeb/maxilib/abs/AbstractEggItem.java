3c1bc724351d67aa711b40e1051a583a787bdcd7
Max094Reikeb <maxencebekier@gmail.com>
2022-05-09 18:07:10 +0200

    Added another abstract class, and more biome methods

---
+++ src/main/java/net/reikeb/maxilib/abs/AbstractEggItem.java
@@ -0,0 +1,74 @@
+package net.reikeb.maxilib.abs;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SpawnEggItem;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class AbstractEggItem extends ForgeSpawnEggItem {
+
+    private final Supplier<? extends EntityType<? extends Mob>> entityType;
+
+    public AbstractEggItem(Supplier<? extends EntityType<? extends Mob>> entityType, int color1, int color2) {
+        super(entityType, color1, color2, new Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC));
+        this.entityType = entityType;
+        DispenserBlock.registerBehavior(this,
+                new DefaultDispenseItemBehavior() {
+                    public ItemStack dispenseStack(BlockSource source, ItemStack stack) {
+                        Direction direction = source.getBlockState().getValue(DispenserBlock.FACING);
+                        EntityType<?> entityType = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
+                        entityType.spawn(source.getLevel(), stack, null, source.getPos().relative(direction), MobSpawnType.DISPENSER, direction != Direction.UP, false);
+                        stack.shrink(1);
+                        return stack;
+                    }
+                });
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        if (level.isClientSide) return InteractionResult.PASS;
+        ItemStack itemStack = context.getItemInHand();
+        BlockPos blockPos = context.getClickedPos();
+        Direction direction = context.getClickedFace();
+        BlockState blockState = level.getBlockState(blockPos);
+        if (level.getBlockEntity(blockPos) instanceof SpawnerBlockEntity spawnerBlockEntity) {
+            BaseSpawner baseSpawner = spawnerBlockEntity.getSpawner();
+            baseSpawner.setEntityId(this.entityType.get());
+            spawnerBlockEntity.setChanged();
+            level.sendBlockUpdated(blockPos, blockState, blockState, Block.UPDATE_CLIENTS + Block.UPDATE_NEIGHBORS);
+            itemStack.shrink(1);
+            return InteractionResult.SUCCESS;
+        }
+        BlockPos blockpos1;
+        if (blockState.getCollisionShape(level, blockPos).isEmpty()) {
+            blockpos1 = blockPos;
+        } else {
+            blockpos1 = blockPos.relative(direction);
+        }
+
+        if (this.entityType.get().spawn((ServerLevel) level, itemStack, context.getPlayer(), blockpos1, MobSpawnType.SPAWN_EGG, true, !Objects.equals(blockPos, blockpos1) && direction == Direction.UP) != null) {
+            itemStack.shrink(1);
+        }
+        return InteractionResult.SUCCESS;
+    }
+}
--- src/main/java/net/reikeb/maxilib/utils/BiomeUtil.java
+++ src/main/java/net/reikeb/maxilib/utils/BiomeUtil.java
@@ -1,10 +1,13 @@
 package net.reikeb.maxilib.utils;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.LinearCongruentialGenerator;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
@@ -12,11 +15,13 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.chunk.PalettedContainer;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.PacketDistributor;
 import net.reikeb.maxilib.MaxiLib;
 import net.reikeb.maxilib.network.NetworkManager;
 import net.reikeb.maxilib.network.packets.BiomeSingleUpdatePacket;
+
+import javax.annotation.Nullable;
+import java.util.function.Predicate;
 
 public class BiomeUtil {
 
@@ -85,6 +90,56 @@ public class BiomeUtil {
             MaxiLib.LOGGER.error(String.format("Failed to replace biome at pos: %s; by biome: %s", pos, biome));
         }
         chunkAccess.setUnsaved(true);
+    }
+
+    /**
+     * Method that finds nearest biome from a position.
+     * WARNING : ONLY RUN THE METHOD SERVER-SIDE!
+     *
+     * @param serverLevel   World where to check for the biome
+     * @param startPosition The position from where we start to search
+     * @param biomeToFind   The biome we want to find
+     * @return Closest position of the biome
+     */
+    @Nullable // This runs server side ONLY, so `ServerLevel` is safe here.
+    private static BlockPos getNearestBiomePosition(ServerLevel serverLevel, BlockPos startPosition, ResourceKey<Biome> biomeToFind) {
+        return getNearestBiomePosition(serverLevel, startPosition, biomeToFind, 6400, 8);
+    }
+
+    /**
+     * Method that finds nearest biome from a position.
+     * WARNING : ONLY RUN THE METHOD SERVER-SIDE!
+     *
+     * @param serverLevel    World where to check for the biome
+     * @param startPosition  The position from where we start to search
+     * @param biomeToFind    The biome we want to find
+     * @param searchRadius   The radius from where we search the biome
+     * @param incrementation Each time the search loops, this increases the xz position from the origin, think of it as a "precision", this will check every 8 blocks, if this is "8", until the search radius is reached.
+     * @return Closest position of the biome
+     */
+    @Nullable // This runs server side ONLY, so `ServerLevel` is safe here.
+    private static BlockPos getNearestBiomePosition(ServerLevel serverLevel, BlockPos startPosition, ResourceKey<Biome> biomeToFind, int searchRadius, int incrementation) {
+        Registry<Biome> biomes = serverLevel.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY);
+        // The holder for biome resource key
+        Holder<Biome> holderToFind = biomes.getHolder(biomeToFind).orElseThrow(() -> new IllegalArgumentException("This biome does not exist in the biome registry!"));
+
+        // Biome doesn't exist in this biome source, therefore we don't want to search (which causes INSANE server side lag) and return null
+        if (!serverLevel.getChunkSource().getGenerator().getBiomeSource().possibleBiomes().contains(holderToFind)) {
+            MaxiLib.LOGGER.error(new TranslatableComponent("commands.locatebiome.notFound"));
+            return null;
+        }
+
+        // We want to check if the holder found in the search matches our biome's holder, effectively the filter that determines when the nearest biome is found.
+        Predicate<Holder<Biome>> holderToFindPredicate = biomeHolder -> biomeHolder == holderToFind;
+
+        Pair<BlockPos, Holder<Biome>> nearestBiome = serverLevel.findNearestBiome(holderToFindPredicate, startPosition, searchRadius, incrementation);
+        // If this returns null, no biome was found.
+        if (nearestBiome == null) {
+            MaxiLib.LOGGER.error(new TranslatableComponent("commands.locatebiome.notFound"));
+            return null;
+        }
+        // Position of the nearest biome.
+        return nearestBiome.getFirst();
     }
 
     /**
